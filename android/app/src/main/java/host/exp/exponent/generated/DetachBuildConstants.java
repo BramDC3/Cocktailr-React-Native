492c2eb4d690474ef7dc062d29298427815acf2d
Bram De Coninck <bram@codifly.be>
2019-05-28 13:47:32 +0200

    Prepare app for second experiment.

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp43e7f6a49084459697c0044a73885a54://192.168.0.178:19000";
+
+}
